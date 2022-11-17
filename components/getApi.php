b17ecf92d19e225960cb66a9048be31c8cd24832
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2022-11-18 04:03:32 +0700

    Update getApi.php

--- components/getApi.php
+++ components/getApi.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Poké-Dex</title>
+    <title>API Example</title>
     <script src="./getApi.js" defer></script>
     <link rel="stylesheet" type="text/css" href="../css/getApi.css" >
     <link rel="icon" type="image/x-icon" href="../images/icon.png">

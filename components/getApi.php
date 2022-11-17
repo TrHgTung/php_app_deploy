6a301ca2e7a0b4a38ef288428ad65967e10a94ea
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2022-11-18 03:11:01 +0700

    upd comm at night

--- components/getApi.php
+++ components/getApi.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 ?>
@@ -47,4 +48,55 @@
     </footer>    
     
 </body>
+=======
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get API Mẫu</title>
+    <script src="../components/getApi.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="../css/getApi.css" >
+    <link rel="icon" type="image/x-icon" href="../images/icon.png">
+</head>
+<body>
+    <!-- FOR LOGGED IN USERS // LOGGED IN SESSION -->
+    <header>
+        <a href="#" class="logo">API mẫu</a>
+        <ul>
+            <li><a href="../index.php" class="active">Trở về Home</a></li>
+        </ul>
+    </header>
+    <section>
+        <img src="../images/stars.png" alt="" id="stars">
+        <img src="../images/mountains_behind.png" alt="" id="mountains_behind">
+        <center>
+            <div class="search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" name="searchBar" id="searchBox" placeholder="Name (max: 100).." >
+                <i class="fa-solid fa-arrow-right"></i>
+            </div>
+        </center>
+        <img src="../images/mountains_front.png" alt="" id="mountains_front">
+    
+    </section>
+   
+
+    <footer class="footer">
+        <h2>Result</h2>
+        <div class="search__container" id="search__containerID">
+            <!--  -->
+        </div>
+        <div class="pokemon__container" id="pokemon__containerID">
+            <!--  -->
+        </div>
+    </footer>    
+    
+</body>
+>>>>>>> c2096d6a001a3fe3ce62feab4673934a4ea8a288
 </html>
---
+++ components/newFeature.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đây là một tính năng</title>
+</head>
+<body>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis nihil vero ipsam sunt temporibus nulla sequi maiores beatae provident dolorem? Aspernatur similique corrupti tempora error itaque voluptate ea esse quisquam!
+</body>
+</html>

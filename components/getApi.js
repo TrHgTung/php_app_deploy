f20a74b4862a97d262f2152f30ad7134b35f1be2
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2022-11-18 03:49:11 +0700

    Update getApi.js

--- components/getApi.js
+++ components/getApi.js
@@ -1,7 +1,14 @@
 const PokemonContainer = document.getElementById("pokemon__containerID");
 const SearchContainer = document.getElementById("search__containerID");
 const SearchElement = document.getElementById("searchBox");
-const PokemonNumber = 100;
+
+/*const SearchElement = document.createElement("input");
+SearchElement.setAttribute("type", "text");
+SearchElement.setAttribute("name", "searchBar");
+SearchElement.setAttribute("placeholder", "Search...");
+SearchContainer.appendChild(SearchElement);*/
+
+const PokemonNumber = 493; /* 493 upto gen4 */
 
 const createPokemonCard = (pokemon) => {
   const PokemonElement = document.createElement("div");
@@ -9,7 +16,10 @@ const createPokemonCard = (pokemon) => {
   PokemonElement.setAttribute("id", PokemonName);
   const PokemonID = pokemon.id;
   const PokemonType = pokemon.types[0].type.name;
-  
+  const PokemonAbility = pokemon.abilities[0].ability.name; /** */
+  const PokemonAtk = pokemon.stats[1].base_stat; /** */
+  const PokemonSpeed = pokemon.stats[5].base_stat;
+  const PokemonSpAtk = pokemon.stats[3].base_stat;
   const PokemonWeight = (pokemon.weight) / 10;
   const PokemonDef = pokemon.stats[2].base_stat;
   const PokemonHP = pokemon.stats[0].base_stat;
@@ -37,6 +47,8 @@ const createPokemonCard = (pokemon) => {
   function checkNum() {
     if (PokemonName < 10) {
       let text = "0";
+
+      //const PokemonNameUnderTen = "0" + PokemonName;
       const PokemonNameUnderTen = text.concat(PokemonName);
       return PokemonNameUnderTen;
 
@@ -52,14 +64,20 @@ const createPokemonCard = (pokemon) => {
   PokemonElement.style.backgroundColor = AddColors;
   const PokemonInnerHTML = `
     <div class="pokemon__imageContainer" id="${PokemonName}">
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${PokemonID}.png" />
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${PokemonID}.png" />
     </div>
     <div class="pokemon__infomationContainer">
       <span class="pokemon__id">#${PokemonID.toString().padStart(3, "0")}</span>
       <h3 class="pokemon__name">${checkNum()}</h3> <!-- changed - PokemonName -->
       <small class="pokemon__type"><b>Hệ: </b><span>${PokemonType}</span></small><br>
+      <small class="pokemon__ability"><b>Đặc tính: </b><span>${PokemonAbility}</span></small><br>
+      <small class="pokemon__Atk"><b>Chỉ số tấn công (cơ bản):</b> <span>${PokemonAtk}</span></small><br>
+      <small class="pokemon_SpAtk"><b>Chỉ số tấn công đặc biệt: </b><span>${PokemonSpAtk}</span></small><br>
+      <small class="pokemon_Speed"><b>Chỉ số tốc độ (cơ bản): </b><span>${PokemonSpeed}</span></small><br>
+      <small class="pokemon_SpAtk"><b>Chỉ số phòng thủ (cơ bản): </b><span>${PokemonDef}</span></small><br>
       <small class="pokemon_Speed"><b>Chỉ số HP: </b><span>${PokemonHP}</span></small><br>
       <small class="pokemon_Speed"><b>Cân nặng: </b><span>${PokemonWeight} kg</span></small><br><br>
+      <input type="button" value="Add to Line-up">
     </div>`;
   PokemonElement.setAttribute("class", "pokemon__card");
   PokemonElement.innerHTML = PokemonInnerHTML;
@@ -96,4 +114,5 @@ const createSearchFilter = (pokemonData) => {
   });
 };
 
-/* https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${PokemonID}.png -- new asset
+/* https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${PokemonID}.png -- new
+https://pokeres.bastionbot.org/images/pokemon/${PokemonID}.png -- old */
